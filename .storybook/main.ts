a253ea6d56084bd061f54334161241d257a76171
Denny Weiß <d.weiss@miljn.com>
2022-07-28 11:21:39 +0200

    Get project structure a little more in sync with `miljn-on-ic` & Use typescript for storybook main

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,33 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
+import type {StorybookConfig} from '@storybook/core-common';
+import {mergeConfig} from "vite";
+import {resolve} from "path";
+
+const config: StorybookConfig = {
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
+    ],
+    framework: "@storybook/vue3",
+    "core": {
+        "builder": "@storybook/builder-vite"
+    },
+    "features": {
+        "storyStoreV7": true
+    },
+    // @ts-ignore
+    async viteFinal(config, options) {
+        return mergeConfig(config, {
+            root: resolve(__dirname, '../src/frontend'),
+            resolve: {
+                alias: {
+                    '~': `${resolve(__dirname, 'src/frontend')}/`,
+                    '@': `${resolve(__dirname, 'src/frontend')}/`,
+                },
+            },
+        })
+    },
 }
+
+module.exports = config
